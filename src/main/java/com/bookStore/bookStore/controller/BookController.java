4464bf1b98bdddb12ea583a9d5e41a70195103b9
Tarcyla Kauane <tarcylakauane7@gmail.com>
2024-06-01 01:59:40 -0300

    Ultima parte do projeto

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -4,12 +4,11 @@ import com.bookStore.bookStore.entity.Book;
 import com.bookStore.bookStore.entity.MyBookList;
 import com.bookStore.bookStore.service.BookService;
 import com.bookStore.bookStore.service.MyBookListService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
@@ -56,6 +55,28 @@ public class BookController {
         return "myBooks";
 
     }
+
+    @RequestMapping("/mylist/(id)")
+    public String getMyList(@PathVariable("id") int id) {
+        Book b=service.getBookById(id);
+        MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+        myBookService.saveMyBooks(mb);
+        return "redirect:/my_books";
+    }
+
+    @RequestMapping("/editBook/{id}")
+    public String editBook(@PathVariable("id") int id,Model model) {
+        Book b=service.getBookById(id);
+        model.addAttribute("book",b);
+        return "bookEdit";
+    }
+
+    @RequestMapping("/deleteBook/{id}")
+    public String deleteBook(@PathVariable("id")int id) {
+        service.deleteById(id);
+        return "redirect:/available_books";
+    }
+
 
 }
 
